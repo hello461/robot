3af0bc91b008edd710cd9e6e7fb40150b952f040
quang <quang2001ht@gmail.com>
2025-12-30 09:49:15 +0700

    Clear go2_control code and add go2_media node

--- src/base/go2_control/src/go2_control.cpp
+++ src/base/go2_control/src/go2_control.cpp
@@ -9,16 +9,6 @@
 
 #define TOPIC_HIGHSTATE "lf/sportmodestate"
 
-enum robotStates{
-    NORMAL_STAND,
-    BALANCE_STAND,
-    STAND_UP,
-    STAND_DOWN,
-    RECOVERY_STAND,
-    MOVE,
-    STOP_MOVE
-};
-
 class Go2SportClientNode : public rclcpp::Node{
 public:
     explicit Go2SportClientNode() :
@@ -26,10 +16,6 @@ public:
             // Read velocity of robot for keyboard or nav2, send it to the robot
             twist_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel", 10,
                                 std::bind(&Go2SportClientNode::twist_callback, this, std::placeholders::_1));
-            
-            // If robot is lying down -> stand up
-            robot_states_sub_ = this->create_subscription<unitree_go::msg::SportModeState>("/lf/sportmodestate", 10,
-                                std::bind(&Go2SportClientNode::robot_state_callback, this, std::placeholders::_1));
         }
 
         void twist_callback(const geometry_msgs::msg::Twist::SharedPtr twist)
@@ -48,25 +34,11 @@ public:
             }
         }
 
-        void robot_state_callback(const unitree_go::msg::SportModeState::SharedPtr sportState){
-            float robotHeight = sportState->body_height;
-            std::cout<<"body height: "<< robotHeight <<std::endl;
-            // stand up: 0.3310, lying down: 0.0738 m
-            if(robotHeight < 0.33){
-                std::cout<<"subscriber shutting down!";
-            }
-        }
-
 private:
-    unitree_go::msg::SportModeState state_;
     // Node: publish command to control the real robot via "/api/sport/request" topic
     SportClient sport_client_;
     // subcribe to "cmd_vel"
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
-    // subscribe to "\lf\sportmodestates"
-    rclcpp::Subscription<unitree_go::msg::SportModeState>::SharedPtr robot_states_sub_;
-    // select robot mode
-    uint8_t robotState_;
     // velcity of the robot
     float x{}, y{}, z{};
     // unitree go2 ros2 request message
